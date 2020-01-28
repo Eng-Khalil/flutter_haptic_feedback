e3a7bf26de8890c1ae8b743748232a4896beacb5
manuel <manuel@gyant.com>
2020-01-28 17:28:59 +0000

    Updated gradle Updated pubspec Remove v1 embedding code Added missing v2 embedding code

--- example/android/app/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback_example/MainActivity.java
+++ example/android/app/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback_example/MainActivity.java
@@ -1,13 +1,16 @@
 package com.gyant.flutter.plugins.flutter_haptic_feedback_example;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+
+import com.gyant.flutter.plugins.flutter_haptic_feedback.FlutterHapticFeedbackPlugin;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
+//    @Override
+//    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+//        super.configureFlutterEngine(flutterEngine);
+//        flutterEngine.getPlugins().add(new FlutterHapticFeedbackPlugin());
+//    }
 }
