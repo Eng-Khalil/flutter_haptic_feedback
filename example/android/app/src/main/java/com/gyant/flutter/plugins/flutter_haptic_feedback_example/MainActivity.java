7abcbd7b163282c61c1c962d753d9a7d3a2bdc05
manuel <manuel@gyant.com>
2020-01-28 17:31:17 +0000

    Removed commented code

--- example/android/app/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback_example/MainActivity.java
+++ example/android/app/src/main/java/com/gyant/flutter/plugins/flutter_haptic_feedback_example/MainActivity.java
@@ -1,16 +1,5 @@
 package com.gyant.flutter.plugins.flutter_haptic_feedback_example;
 
-
-import com.gyant.flutter.plugins.flutter_haptic_feedback.FlutterHapticFeedbackPlugin;
-
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
 
-public class MainActivity extends FlutterActivity {
-
-//    @Override
-//    public void configureFlutterEngine(FlutterEngine flutterEngine) {
-//        super.configureFlutterEngine(flutterEngine);
-//        flutterEngine.getPlugins().add(new FlutterHapticFeedbackPlugin());
-//    }
-}
+public class MainActivity extends FlutterActivity { }
